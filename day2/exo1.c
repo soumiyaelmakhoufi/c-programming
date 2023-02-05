71d2c6a409cb21d997583214a849099c9d63ce1a
soumiyaelmakhoufi <soumiya.ELMAKHLOUFI@um6p.ma>
2023-02-05 20:51:30 +0100

    exercice1_day2

---
+++ day2/exo1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+  float a ,b ,leur_produit;
+    printf("veuillez entrer la valeur de a :\n");
+  scanf("%f",&a);
+  printf("veuillez entrer la valeur de b :\n");
+  scanf("%f",&b);
+  leur_produit=a*b;
+  printf("le produit de a et b vaut:%.3f\n",leur_produit);
+  return 0;
+}
+  
+  
